48f1f1a1adea1db1c5729e8a8a42fdd6f367bb56
Bruno <bleiteb@indracompany.com>
2022-02-07 09:22:11 -0300

    Adicionado arquivo de testes

--- src/test/java/com/example/demo/DockApplicationTests.java
+++ src/test/java/com/example/demo/DockApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.dock.test.domain.Terminal;
+
 class DockApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Terminal terminal = new Terminal();
+		
+		Assert.assertTrue(true);
 	}
 
 }
